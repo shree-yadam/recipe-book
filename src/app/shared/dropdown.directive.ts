82262534c30bdd95b42a40f5794cf769f5506048
Shree Devi Yadam <shree.yadam@gmail.com>
2021-11-19 21:39:54 -0500

    custom directive to open and close manage recipe button

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
+
+@Directive({
+  selector: '[appDropDown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') clicked: boolean = false;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2){ }
+
+  @HostListener('click') dropDown(eventData: Event) {
+    this.clicked = !this.clicked;
+  }
+}
